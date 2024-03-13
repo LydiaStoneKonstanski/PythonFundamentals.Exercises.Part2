0687a265cc10c5f4aa428a521cb3b30329caf4ec
LydiaStoneKonstanski <yourname@example.com>
2024-03-12 22:17:30 -0400

    Greet

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,9 +1,9 @@
 def greet(name):
-    print(greet("Hi " + name + ", you're the best!"))
+    print("Hi " + name + ", you're the best!")
     return
 
 greet("Lydia");
 
-greet("Dart Vader");
+greet("Darth Vader");
 
 greet("Guido van Rossum");
